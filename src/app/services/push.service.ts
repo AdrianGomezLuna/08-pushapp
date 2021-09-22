ea6f69ce7fd9987c5280f19c4475cf8c78f19c9e
AdrianGomezLuna <adrigoluna@gmail.com>
2021-09-22 17:36:36 +0200

    Mostrar notificaciones recibidas

--- src/app/services/push.service.ts
+++ src/app/services/push.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { OneSignal } from '@ionic-native/onesignal/ngx';
+import { OneSignal, OSNotification } from '@ionic-native/onesignal/ngx';
 
 
 @Injectable({
@@ -7,17 +7,25 @@ import { OneSignal } from '@ionic-native/onesignal/ngx';
 })
 export class PushService {
 
+  mensajes: any[] = [
+    {
+      title: 'Titulo de la Push',
+      body: 'Este es el body de la Push',
+      date: new Date()
+    }
+  ];
+
   constructor(private oneSignal: OneSignal) { }
 
   configuracionInicial() {
     this.oneSignal.startInit('093f0449-d95a-4688-9dbb-e5789efb5d69', '981047451701');
 
-    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
 
     this.oneSignal.handleNotificationReceived().subscribe((noti) => {
     // do something when notification is received
       console.log('Notificacion recibido', noti);
-
+      this.notificacionRecibida(noti);
     });
 
     this.oneSignal.handleNotificationOpened().subscribe((noti ) => {
@@ -28,4 +36,18 @@ export class PushService {
 
     this.oneSignal.endInit();
   }
+
+  notificacionRecibida(noti: OSNotification) {
+
+    const payload = noti.payload;
+    const existePush = this.mensajes.find( mensaje => mensaje.notificactionID === payload.notificationID);
+
+    if (existePush) {
+      return;
+    }
+
+    this.mensajes.unshift( payload );
+  }
+
+
 }
